87ea47156d2802a8482ea676159aecb5d447bda9
Mark Aikens <markadev@primeletters.net>
2016-09-06 07:31:44 -0400

    Add a license

--- setup.py
+++ setup.py
@@ -2,9 +2,13 @@ from setuptools import find_packages, setup
 
 
 setup(
-    name = 'meldcache',
+    name = 'python-meldcache',
     description = 'Memcached client that melds multiple memcached servers ' +
                   'into one elastic cache cluster.',
+    author = 'Mark Aikens',
+    author_email = 'markadev@primeletters.net',
+    license = 'MIT',
+
     packages = find_packages(),
     install_requires = [
         'pymemcache',
